a54801bd67a2c042531e6ba198d31f832a70ff4e
Israel Gonzalez <emeraldaspx@gmail.com>
2016-11-02 01:22:02 +0000

    comments

---
+++ app.js
@@ -0,0 +1,26 @@
+/*global $*/
+// state object used instead of the DOM to store the application state
+var item = {
+  name: 1,
+  checked: true
+};
+var state = {
+  items: [item]
+};
+// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+
+
+// listen for form submission
+
+
+// grab text in form after submission
+
+
+// create item and add to list
+
+
+// check and uncheck items on the list by clicking the "Check" button
+
+
+// permanently remove items from the list
+console.log(state);
